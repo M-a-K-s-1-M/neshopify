2f6dd8d03443d160910680ae97ad4e69429d9857
Емельянов Максим <masiko-17@mail.ru>
2025-11-07 01:32:16 +0500

    Добавит настройку colors. fontSizes, spcing в тему

--- admin-frontend/src/app/providers/theme.tsx
+++ admin-frontend/src/app/providers/theme.tsx
@@ -1,45 +1,58 @@
-import { createTheme, MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider, virtualColor } from "@mantine/core";
 import React from "react";
 
+
 const theme = createTheme({
+    // primaryColor: 'gray',
+    // primaryShade: { dark: 6, light: 6 },
+
     fontFamily: 'system-ui, Avenir, Helvetica, Arial, sans-serif',
+    autoContrast: true, // Автоматически меняет цвет текста на черный/белый, в зависимости от яркости фона
+    luminanceThreshold: 0.3, // Если яркость цвета > 0.3 - текст черный, иначе белый
 
-    autoContrast: true,
-    luminanceThreshold: 0.3,
+    fontSizes: {
+        "2xl": '1.5rem',
+        "3xl": '1.75rem',
+    },
 
-    primaryShade: { dark: 6, light: 6 },
+    spacing: {
+        "2xl": '2.5rem',
+        "3xl": '3.25rem'
+    },
 
-    // colors: {
-    //     // Синяя палитра для светлой темы
-    //     blue: [
-    //         '#f5f8ff', '#e1ebff', '#c7dbff', '#a3c4ff',
-    //         '#7ba9ff', '#4d8dff', '#2d7cff', '#1a6ae6',
-    //         '#0d5ccc', '#004bb3'
-    //     ],
+    // accent, primary, secondary, hover, error, success
+    colors: {
 
-    //     // Серые палитры
-    //     dark: [
-    //         '#C1C2C5', '#A6A7AB', '#909296', '#5C5F66',
-    //         '#373A40', '#2C2E33', '#25262B', '#1A1B1E',
-    //         '#141517', '#101113'
-    //     ],
+        background: virtualColor({
+            name: 'background',
+            dark: 'gray',
+            light: 'gray',
+        }),
 
-    //     gray: [
-    //         '#f8fafc', '#f1f5f9', '#e2e8f0', '#cbd5e1',
-    //         '#94a3b8', '#64748b', '#475569', '#334155',
-    //         '#1e293b', '#0f172a'
-    //     ],
+        backgroundSecondary: virtualColor({
+            name: 'background-secondary',
+            dark: 'gray',
+            light: 'gray',
+        }),
 
-    //     primary: virtualColor({
-    //         name: 'primary',
-    //         dark: 'blue',
-    //         light: 'gray',
-    //     }),
-    // },
+        text: virtualColor({
+            name: 'text',
+            dark: 'gray',
+            light: 'gray',
+        }),
 
-    // primaryColor: 'primary',
+        textSecondary: virtualColor({
+            name: 'textSecondary',
+            dark: 'gray',
+            light: 'gray',
+        }),
 
-
+        button: virtualColor({
+            name: 'button',
+            dark: 'gray',
+            light: 'gray',
+        })
+    }
 })
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
