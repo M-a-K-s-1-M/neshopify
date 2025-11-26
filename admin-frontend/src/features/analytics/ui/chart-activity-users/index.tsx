9203da6037bb4fc382b398cbe0b76499887d34b9
M-a-K-s-1-M <masiko-17@mail.ru>
2025-11-26 11:26:57 +0500

    Создаст скелетон загрузки для блоков аналитики и для графиков

--- admin-frontend/src/features/analytics/ui/chart-activity-users/index.tsx
+++ admin-frontend/src/features/analytics/ui/chart-activity-users/index.tsx
@@ -1,26 +1,41 @@
 import { userActivityData } from "@/shared";
 import { BarChart } from "@mantine/charts";
-import { Box, Text } from "@mantine/core";
+import { Box, Skeleton, Text } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
 
 export function ChartActivityUsers() {
+
+    const { data, isPending, isError } = useQuery({
+        queryKey: ['userActivityChart'],
+        queryFn: async () => {
+            return userActivityData;
+        }
+    })
+
+    if (isError) return <div>Ошибка загрузки данных</div>;
+
     return (
         <Box component="div" p={'md'}>
             <Text fw={500} size="lg" mb={'xs'}>Активность пользователей</Text>
 
-            <BarChart
-                p={'md'}
-                h={300}
-                w={{ base: 300, sm: '100%' }}
-                data={userActivityData}
-                dataKey="month"
-                type="stacked"
-                tickLine="y"
-                series={[
-                    { name: 'ActiveUsers', color: 'teal', label: 'Активные пользователи' },
-                    { name: 'NewRegistrations', color: 'violet', label: 'Новые регистрации' },
-                    { name: 'Sessions', color: 'cyan', label: 'Сессии' },
-                ]}
-            />
+            {isPending ?
+                <Skeleton height={300} w={{ base: 300, sm: '100%' }} />
+                :
+                <BarChart
+                    p={'md'}
+                    h={300}
+                    w={{ base: 300, sm: '100%' }}
+                    data={data}
+                    dataKey="month"
+                    type="stacked"
+                    tickLine="y"
+                    series={[
+                        { name: 'ActiveUsers', color: 'teal', label: 'Активные пользователи' },
+                        { name: 'NewRegistrations', color: 'violet', label: 'Новые регистрации' },
+                        { name: 'Sessions', color: 'cyan', label: 'Сессии' },
+                    ]}
+                />
+            }
         </Box>
     )
 }
