c3c01e365d57e087369c730a2e5b38d7c91b8e32
Емельянов Максим <masiko-17@mail.ru>
2025-11-26 23:47:22 +0500

    Создаст сервис для создания пользователя

--- new-backend/src/modules/users/users.service.ts
+++ new-backend/src/modules/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, User } from 'generated/prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Injectable()
 export class UsersService {
@@ -10,4 +12,22 @@ export class UsersService {
         return await this.prisma.user.findMany({ include: { roles: true } });
     }
 
+    async create(dto: CreateUserDto): Promise<Prisma.UserGetPayload<{ include: { roles: true } }>> {
+        const { email, password, roles } = dto;
+        const passwordHash = await bcrypt.hash(password, 10);
+        return await this.prisma.user.create({
+            data: {
+                email: email,
+                passwordHash: passwordHash,
+                roles: {
+                    connect: roles.map(role => ({ value: role }))
+                }
+            },
+            include: { roles: true }
+        });
+    }
+
+    async getByEmail(email: string): Promise<User | null> {
+        return await this.prisma.user.findUnique({ where: { email } });
+    }
 }
