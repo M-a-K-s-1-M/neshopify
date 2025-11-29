4ac0c67d078d8200849ed89881a39def6c45772c
Емельянов Максим <masiko-17@mail.ru>
2025-11-29 05:55:11 +0500

    Добавит валидацию для UpdateUserDto

--- new-backend/src/modules/users/dto/update-user.dto.ts
+++ new-backend/src/modules/users/dto/update-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsArray, IsEmail, IsOptional, IsString, Length } from "class-validator";
+
 export class UpdateUserDto {
+    @IsEmail({}, { message: 'Неверный формат email' })
     readonly email: string;
+
+    @IsOptional()
+    @IsString({ message: 'Пароль должен быть строкой' })
+    @Length(6, 20, { message: 'Пароль должен быть от 6 до 20 символов' })
     readonly password?: string;
+
+    @IsArray({ message: 'Роли должны быть массивом' })
     readonly roles: string[];
 }
