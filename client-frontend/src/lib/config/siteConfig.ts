b349f7274d2f2704bf371c36857ea702731b4b34
Емельянов Максим <masiko-17@mail.ru>
2025-12-12 17:05:36 +0500

    Исправит ошибку сборки проекта

--- client-frontend/src/lib/config/siteConfig.ts
+++ client-frontend/src/lib/config/siteConfig.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
     name: "Cosmiq",
     description: "Создайте свой интернет - магазин за минуту",
+    colorIcon: "",
 
     headerLinks: [
         { name: "Мои сайты", href: "/sites" },
